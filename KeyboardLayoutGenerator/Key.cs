a9800af1c10478c447022f011cb67660ae8e4767
Sebastian Wittmann <sebastian.wittmann97zibbo@gmail.com>
2019-10-03 00:40:06 +0200

    added key base constructor

--- KeyboardLayoutGenerator/Key.cs
+++ KeyboardLayoutGenerator/Key.cs
@@ -53,5 +53,51 @@ namespace KeyboardLayoutGenerator
         private Color _colorFrontLeft;
         private Color _colorFrontCenter;
         private Color _colorFrontRight;
+
+        public Key()
+        {
+            this._size = 3.0;
+            this._color = Color.FromArgb(0, 0, 0);
+            this._keyColor = Color.FromArgb(204, 204, 204);
+            this._width = 1.0;
+            this._height = 1.0;
+            this._width2 = 1.0;
+            this._height2 = 1.0;
+            this._x = 0.0;
+            this._y = 0.0;
+            this._x2 = 0.0;
+            this._y2 = 0.0;
+
+            this._stepped = false;
+            this._homing = false;
+
+            this._topLeft = "";
+            this._topCenter = "";
+            this._topRight = "";
+            this._colorTopLeft = Color.FromArgb(0, 0, 0);
+            this._colorTopCenter = Color.FromArgb(0, 0, 0);
+            this._colorTopRight = Color.FromArgb(0, 0, 0);
+
+            this._centerLeft = "";
+            this._centerCenter = "";
+            this._centerRight = "";
+            this._colorCenterLeft = Color.FromArgb(0, 0, 0);
+            this._colorCenterCenter = Color.FromArgb(0, 0, 0);
+            this._colorCenterRight = Color.FromArgb(0, 0, 0);
+
+            this._bottomLeft = "";
+            this._bottomCenter = "";
+            this._bottomRight = "";
+            this._colorBottomLeft = Color.FromArgb(0, 0, 0);
+            this._colorBottomCenter = Color.FromArgb(0, 0, 0);
+            this._colorBottomRight = Color.FromArgb(0, 0, 0);
+
+            this._frontLeft = "";
+            this._frontCenter = "";
+            this._frontRight = "";
+            this._colorFrontLeft = Color.FromArgb(0, 0, 0);
+            this._colorFrontCenter = Color.FromArgb(0, 0, 0);
+            this._colorFrontRight = Color.FromArgb(0, 0, 0);
+        }
     }
 }
