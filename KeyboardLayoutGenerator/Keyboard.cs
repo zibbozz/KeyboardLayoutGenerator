7599da71b1914c87c1714bc61d88293ca1b3d809
Sebastian Wittmann <sebastian.wittmann97zibbo@gmail.com>
2019-10-03 00:27:33 +0200

    added keyboard class + private base vars

---
+++ KeyboardLayoutGenerator/Keyboard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace KeyboardLayoutGenerator
+{
+    public class Keyboard
+    {
+        private List<Key> _keys;
+        private Color _backgroundColor;
+        private string _title;
+        private string _author;
+    }
+}
